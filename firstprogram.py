b9b03c1b94f0a5dceaa0afe858543f1bd78f45bd
shraddha-ravankar <ravankarshraddha11@gmail.com>
2024-07-06 13:26:49 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,17 @@
+# #input from the user
+a=int(input("enter first numeber:"))
+b=int(input("enter second number:"))
+sum=a+b
+print(sum)
+print(type(a))
+
+a=11
+f=float(a)
+print(a)
+b=hex(a)
+print(b)
+c=oct(a)
+print(a)
+char='A'
+r=ord(char)
+print(r)
